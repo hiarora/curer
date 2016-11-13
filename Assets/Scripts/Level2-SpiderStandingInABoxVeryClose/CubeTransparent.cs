7ebdfac171e0e0a9c474b936d14be35ab5b72958
Himanshu Arora <himanshuarora.1907@gmail.com>
2016-11-12 18:54:26 -0800

    Level 2: Spider within a transparent cube in a close range. Adding .gitignore

---
+++ Assets/Scripts/Level2-SpiderStandingInABoxVeryClose/CubeTransparent.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.IO;
+
+public class CubeTransparent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//this.gameObject.addComponent("Renderer").material = new Material(Shader.Find("Transparent/Diffuse"));
+		Renderer r = this.gameObject.GetComponent<MeshRenderer>();
+		Debug.Log(Shader.Find("Transparent/Diffuse"));
+		r.material = new Material(Shader.Find("Transparent/Diffuse"));
+        r.material.color = new Color(1, 1, 1, 0.4f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
