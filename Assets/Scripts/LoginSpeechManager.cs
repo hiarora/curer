5cbe96cb7b3598b43529b3e874383b4e42713fc1
Himanshu Arora <himanshuarora.1907@gmail.com>
2016-11-26 13:30:45 -0800

    Login scene added. A user now has to enter our cms to enable user analytics and progress.

---
+++ Assets/Scripts/LoginSpeechManager.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+using System.Net;
+using System.IO;
+
+
+ 
+public class LoginSpeechManager : MonoBehaviour
+{
+	public class CoroutineWithData {
+     public Coroutine coroutine { get; private set; }
+     public object result;
+     private IEnumerator target;
+     public CoroutineWithData(MonoBehaviour owner, IEnumerator target) {
+         this.target = target;
+         this.coroutine = owner.StartCoroutine(Run());
+     }
+ 
+     private IEnumerator Run() {
+         while(target.MoveNext()) {
+             result = target.Current;
+             yield return result;
+         }
+     }
+	}
+	private string token; 
+    KeywordRecognizer keywordRecognizer = null;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();	
+	
+	IEnumerator GetToken(System.Action<string> callBack) {
+		WWW w = new WWW("http://ec2-54-183-221-129.us-west-1.compute.amazonaws.com/phobiacurer/getRandomToken.php");
+		yield return w;
+		callBack(w.text);
+	}
+	
+	IEnumerator ValidateTokenForLogIn(System.Action<string> callBack, string token) {
+		WWW w = new WWW("http://ec2-54-183-221-129.us-west-1.compute.amazonaws.com/phobiacurer/validateToken.php?token=" + token);
+		yield return w;
+		callBack(w.text);
+	}
+	
+	public void ValidateToken() {
+		GameObject.Find("Start").GetComponent<Text>().text = "Validating token...";
+		StartCoroutine(ValidateTokenForLogIn(callBack => {
+        // callBack is going to be null until it’s set
+			if(callBack != null) {
+            // Now callBack acts as an int
+				if(callBack == "Valid")
+					Application.LoadLevel(1);
+				else
+					GameObject.Find("Start").GetComponent<Text>().text = "Invalid. Please make sure you have logged in.";
+			}
+		}, this.token));
+	}
+	
+	public void FetchToken() {
+		GameObject.Find("LoginText").GetComponent<Text>().text = "Fetching token. Please wait ...";
+		StartCoroutine(GetToken(callBack => {
+        // callBack is going to be null until it’s set
+			if(callBack != null) {
+            // Now callBack acts as an int
+				GameObject.Find("LoginText").GetComponent<Text>().text = "Enter token." + callBack;
+				this.token = callBack;
+				GameObject.Find("Start").GetComponent<Text>().text = "Say start to continue";
+			}
+		}));
+	}
+
+    // Use this for initialization
+    void Start()
+    {
+		/*keywords.Add("Token", () =>
+        {
+			//loadingImage.SetActive(true);
+			FetchToken();
+        });
+		// Tell the KeywordRecognizer about our keywords.
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+
+        // Register a callback for the KeywordRecognizer and start recognizing!
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();*/
+    }
+
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        /*System.Action keywordAction;
+        if (keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }*/
+    }
+}
--- Assets/Scripts/PhobiaSpeechManager.cs
+++ Assets/Scripts/PhobiaSpeechManager.cs
@@ -1,17 +1,92 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Windows.Speech;
+using System.Net;
+using System.IO;
 
 public class PhobiaSpeechManager : MonoBehaviour
 {
-    KeywordRecognizer keywordRecognizer = null;
+	private string token;
+     public class CoroutineWithData {
+     public Coroutine coroutine { get; private set; }
+     public object result;
+     private IEnumerator target;
+     public CoroutineWithData(MonoBehaviour owner, IEnumerator target) {
+         this.target = target;
+         this.coroutine = owner.StartCoroutine(Run());
+     }
+ 
+     private IEnumerator Run() {
+         while(target.MoveNext()) {
+             result = target.Current;
+             yield return result;
+         }
+     }
+	}
+ 
+ 	IEnumerator GetToken(System.Action<string> callBack) {
+		WWW w = new WWW("http://ec2-54-183-221-129.us-west-1.compute.amazonaws.com/phobiacurer/getRandomToken.php");
+		yield return w;
+		callBack(w.text);
+	}
+	
+	
+	public void FetchToken() {
+		GameObject.Find("LoginText").GetComponent<Text>().text = "Fetching token. Please wait ...";
+		StartCoroutine(GetToken(callBack => {
+        // callBack is going to be null until it’s set
+			if(callBack != null) {
+            // Now callBack acts as an int
+				GameObject.Find("LoginText").GetComponent<Text>().text = "Enter token." + callBack;
+				this.token = callBack; 
+				GameObject.Find("Start").GetComponent<Text>().text = "Say start to continue";
+			}
+		}));
+	}
+	
+	IEnumerator ValidateTokenForLogIn(System.Action<string> callBack, string token) {
+		WWW w = new WWW("http://ec2-54-183-221-129.us-west-1.compute.amazonaws.com/phobiacurer/validateToken.php?token=" + token);
+		yield return w;
+		callBack(w.text);
+	}
+	
+	public void ValidateToken() {
+		GameObject.Find("Start").GetComponent<Text>().text = "Validating token...";
+		StartCoroutine(ValidateTokenForLogIn(callBack => {
+        // callBack is going to be null until it’s set
+			if(callBack != null) {
+            // Now callBack acts as an int
+				if(callBack == "Valid")
+					Application.LoadLevel(1);
+				else
+					GameObject.Find("Start").GetComponent<Text>().text = "Invalid. Please make sure you have logged in.";
+			}
+		}, this.token));
+	}
+	
+	KeywordRecognizer keywordRecognizer = null;
 	public GameObject loadingImage;
     Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
 
     // Use this for initialization
     void Start()
     {
+		keywords.Add("Token", () =>
+        {
+			//loadingImage.SetActive(true);
+			FetchToken();
+        });
+		
+		keywords.Add("Start", () =>
+        {
+			//loadingImage.SetActive(true);
+			ValidateToken();
+        });
+		
 		keywords.Add("One", () =>
         {
 			//loadingImage.SetActive(true);
